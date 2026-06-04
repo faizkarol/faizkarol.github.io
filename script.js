0d5be6692ebba0bcddc5e8ca208198e55ed19c42
Faiz Karol <faizkarol78@gmail.com>
2026-06-04 18:43:03 +0200

    Enhance theme toggle functionality
    Update theme toggle button to change text based on theme.

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const btn = document.getElementById("theme-toggle");
 
 btn.addEventListener("click", () => {
-document.body.classList.toggle("light");
+
+    document.body.classList.toggle("light");
+
+    if(document.body.classList.contains("light")){
+        btn.textContent = "☀️";
+    }else{
+        btn.textContent = "🌙";
+    }
+
 });
